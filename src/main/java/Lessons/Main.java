6eb16f27d4776b1d8ab7aaae6123255e7873d55d
Patryk Ostrowski <ostrowski.patryk@gmail.com>
2023-12-25 19:38:32 +0100

    Modules for inheritance and lessons added.

--- src/main/java/Lessons/Main.java
+++ src/main/java/Lessons/Main.java
@@ -15,8 +15,23 @@ public class Main {
 
         //3 - third exercise: display values concatenated
         System.out.println("Byte variable value is " + value1);
+        //byte range: -128 to 127
+        //short range: -32 768 to 32 767
+        //int range: -2 147 483 648 to 2 147 483 647
+        //long range: -9 223 372 036 854 775 808 to 9 223 372 036 854 775 807
 
         //4 - fourth exercise: 'if' conditional statement
+        int valueToCheck = 128;
+
+        if (valueToCheck <= 127) {
+            System.out.println("The number type is byte.");
+        }
+
+        if (valueToCheck <= 127) {
+            System.out.println("The number type is byte.");
+        } else {
+            System.out.println("The numer type is integer.");
+        }
 
 
 
