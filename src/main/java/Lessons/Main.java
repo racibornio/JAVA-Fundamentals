c937bc2ab1b800b2c41517dd41c928cabdf433c2
Patryk <ostrowski.patryk@gmail.com>
2023-12-25 15:13:07 +0100

    Var types adding.

--- src/main/java/Lessons/Main.java
+++ src/main/java/Lessons/Main.java
@@ -2,7 +2,16 @@ package Lessons;
 
 public class Main {
     public static void main(String[] args) {
+
         //1 - first exercise: type 'sout'
         System.out.println("It works!");
+
+        //2 - second exercise: variables
+        byte value1 = 3;
+        short value2 = 2_000;
+        int value3 = 4_000_000;
+        long value4 = 490_000_000;
+        long value5 = 490_000_000_000_000L;
+
     }
 }
